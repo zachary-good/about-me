f2181d081cf68d85857d6413d1b34444b0981b07
Zachary Good <zjgood13@gmail.com>
2021-08-11 17:39:43 -0700

    added backside for new questions, updated css styling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//alert('Hello!');
+var userScore = 0;
 
 let userName = prompt('Hello user, what is your name?');
 console.log(userName);
@@ -12,6 +12,7 @@ let answer1Lower = answer1.toLowerCase();
 
 if(answer1Lower === 'yes' || answer1Lower === 'y'){
   alert('Thats correct Zack loves ice cream!');
+  userScore++;
   }
   else if(answer1Lower === 'no' || answer1Lower === 'n'){
     alert('You are wrong')
@@ -26,6 +27,7 @@ let answer2Lower = answer2.toLowerCase();
 
 if(answer2Lower === 'no' || answer2Lower === 'n'){
   alert('Thats correct Zack hates sitting inside all day!');
+  userScore++;
   }
  else if(answer2Lower === 'yes' || answer2Lower === 'y'){
   alert('You are wrong')
@@ -40,6 +42,7 @@ let answer3Lower = answer3.toLowerCase();
 
 if(answer3Lower === 'yes' || answer3Lower === 'y'){
   alert('Thats correct Zack loves long walks on the beach!');
+  userScore++;
  }
  else if(answer3Lower === 'no' || answer3Lower === 'n'){
   alert('You are wrong')
@@ -54,7 +57,9 @@ let answer4Lower = answer4.toLowerCase();
 
 if(answer4Lower === 'no' || answer4Lower === 'n'){
   alert('Thats correct Zack does not like getting stung! Come no now who does?');
- }else if(answer4Lower === 'yes' || answer4Lower === 'y'){
+  userScore++;
+ }
+ else if(answer4Lower === 'yes' || answer4Lower === 'y'){
   alert('You are wrong')
   }
   else{
@@ -67,6 +72,7 @@ let answer5Lower = answer5.toLowerCase();
 
 if(answer5Lower === 'yes' || answer5Lower === 'y'){
   alert('Thats correct Zack is very much looking forward to all he is going to learn!');
+  userScore++;
  }
  else if(answer5Lower === 'no' || answer5Lower === 'n'){
   alert('You are wrong')
@@ -75,4 +81,68 @@ if(answer5Lower === 'yes' || answer5Lower === 'y'){
     alert('Come on you should know better! Everyone loves learning!');
 }
 
-alert(userName + ' thanks for coming to learn more about Zack, feel free to stick around and read as much as you like!');
+var number = 5;
+var answer = false;
+
+
+for(var i = 0; i < 4; i++){
+  var numGuess = prompt('Please guess a number between 1 and 20!');
+  console.log(numGuess);
+  //checks that number is within range
+  while(numGuess < 1 || numGuess > 20){
+    var numGuess = prompt('That number was out of range, please pick a number between 1 and 20:');
+    console.log(numGuess);
+  }
+
+  if(numGuess < number){
+    alert('Your guess was too low!');
+  }
+  if(numGuess > number){
+    alert('your guess was too high!');
+  }
+  //check if the answer matches the guess and switches statement to true to exit loop
+  if(number == numGuess){
+    alert('You are correct the answer was five!');
+    var answer = true;
+    userScore++;
+   break;
+  }
+}
+if(answer == false){
+alert('sorry you did not get the answer, it was five.')
+}
+
+const favFoods = ["pizza", "hamburger", "ice cream"];
+console.log(favFoods);
+var foodAns = false;
+
+for(var j = 0; j < 6; j++){
+
+  var foodGuess = prompt('What do you think is Zacks favorite food?');
+  var foodGuessLower = foodGuess.toLowerCase();
+  console.log(foodGuessLower);
+
+  for(var i = 0; i < favFoods.length; i++){
+
+    if(favFoods[i] === foodGuessLower){
+      alert('Congrats you guessed zacks favorite food!');
+      foodAns = true;
+      userScore++;
+      break;
+    }
+  }
+  if(foodAns === true){
+    break;
+  }
+  if(foodAns === false){
+  alert('That was an incorrect guess, guess again.');
+  }
+}
+
+alert('these are zacks favorite foods:');
+
+for(var i = 0; i < favFoods.length; i++){
+  alert(favFoods[i]);
+}
+
+alert(userName + ' thanks for coming to learn more about Zack. Your Zack score was ' + userScore + '/7. feel free to stick around and read as much as you like!');
